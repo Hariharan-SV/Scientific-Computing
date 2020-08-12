f30f73180476d8da4e22192e80cd1085854d8137
Hariharan20 <harikumar20112000@gmail.com>
2020-08-12 18:04:43 +0530

    Iterative Methods/gauss_seidall.py: Add gauss seidall code

---
+++ Iterative Method for Linear Equation/gauss_seidall.py
@@ -0,0 +1,19 @@
+import get_coefficients_as_list
+
+# function that computes in gauss seidall method
+def gauss_seidall(no_of_unknowns):
+  coefficient_list = get_coefficients_as_list.get_coefficients_as_list(no_of_unknowns)
+  factors = [1]+[0]*(no_of_unknowns-2)+[1]
+  sample_factors = [1]+[0]*(no_of_unknowns-2)+[1]
+  for i in range(0,6):
+    for j in range(0,no_of_unknowns):
+      diff = 0
+      for k in range(0,j):
+        diff = diff + coefficient_list[j][k]*sample_factors[k]
+      for k in range(j+1,no_of_unknowns):
+        diff = diff + coefficient_list[j][k]*sample_factors[k]
+      diff = (coefficient_list[j][no_of_unknowns]-diff)/coefficient_list[j][j]
+      sample_factors = sample_factors[0:j]+[diff]+sample_factors[j+1:]
+      print("Error % is ",abs(100*(factors[j]-sample_factors[j])/sample_factors[j]))
+    factors = sample_factors
+    print("At iteration ",i+1," factors are ",sample_factors)
