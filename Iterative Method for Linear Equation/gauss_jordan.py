e8cf2b00cb60cffdadb293e02c9918b41948fd0e
Hariharan20 <harikumar20112000@gmail.com>
2020-08-12 20:10:46 +0530

    Iterative Methods/gauss_jordan: Create solution for gauss_jordan method

---
+++ Iterative Method for Linear Equation/gauss_jordan.py
@@ -0,0 +1,19 @@
+import get_coefficients_as_list
+
+# function that computes in gauss jardon method
+def gauss_jordan(no_of_unknowns):
+  factors = [0]*no_of_unknowns
+  coefficient_list = get_coefficients_as_list.get_coefficients_as_list(no_of_unknowns)
+  for i in range(0,no_of_unknowns):
+    if int(coefficient_list[i][i]) == 0:
+      print('Divide by zero detected!')
+      return
+    for j in range(0,no_of_unknowns):
+      if i != j:
+        ratio = coefficient_list[j][i]/coefficient_list[i][i]
+        for k in range(0,no_of_unknowns+1):
+          coefficient_list[j][k] = coefficient_list[j][k] - ratio * coefficient_list[i][k]
+  # Obtaining Solution
+  for i in range(0,no_of_unknowns):
+    factors[i] = coefficient_list[i][no_of_unknowns]/coefficient_list[i][i]
+  print(factors)
