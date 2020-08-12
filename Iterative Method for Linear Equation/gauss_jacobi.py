bba549dbd0e1995582c3cf2f308f5254377f1a64
Hariharan20 <harikumar20112000@gmail.com>
2020-08-12 20:10:05 +0530

    Iterative Methods/gauss_jacobi.py: Create solution for gaus_jacobi method

---
+++ Iterative Method for Linear Equation/gauss_jacobi.py
@@ -0,0 +1,19 @@
+import get_coefficients_as_list
+
+# function that computes in gauss jacobi method
+def gauss_jacobi(no_of_unknowns):
+  coefficient_list = get_coefficients_as_list.get_coefficients_as_list(no_of_unknowns)
+  factors = [0]*(no_of_unknowns)
+  sample_factors = [0]*(no_of_unknowns)
+  for i in range(0,6):
+    for j in range(0,no_of_unknowns):
+      diff = 0
+      for k in range(0,j):
+        diff = diff + coefficient_list[j][k]*factors[k]
+      for k in range(j+1,no_of_unknowns):
+        diff = diff + coefficient_list[j][k]*factors[k]
+      #print(coefficient_list[j][no_of_unknowns],"-",diff,"/",coefficient_list[j][j])
+      diff = (coefficient_list[j][no_of_unknowns]-diff)/coefficient_list[j][j]
+      sample_factors = sample_factors[0:j]+[diff]+sample_factors[j+1:]
+    factors = sample_factors
+    print("At iteration ",i+1," factors are ",factors)
